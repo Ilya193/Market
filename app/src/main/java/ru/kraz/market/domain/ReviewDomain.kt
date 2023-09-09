cdf5d0f8a2eddb2d6132922f08f8872452a7fa27
ilyapc <ilya.komendantov@list.ru>
2023-09-09 20:26:45 +0300

    changing mapper logic

--- app/src/main/java/ru/kraz/market/domain/ReviewDomain.kt
+++ app/src/main/java/ru/kraz/market/domain/ReviewDomain.kt
@@ -1,9 +1,5 @@
 package ru.kraz.market.domain
 
-import ru.kraz.market.core.ToMapper
-import ru.kraz.market.presentation.ProductUi
-import ru.kraz.market.presentation.ReviewUi
-
 data class ReviewDomain(
     val id: Int,
     val textReview: String,
